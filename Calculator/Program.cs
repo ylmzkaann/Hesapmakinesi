13e84e2385919e13479911940a8cfbdbf60f1a6a
Kaan <ylmzkaann99@gmail.com>
2025-07-21 17:25:45 +0300

    test comment 3

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,59 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Calculator calc = new Calculator();
-
-            Console.Write("Birinci sayıyı gir: ");
-            double x = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("İkinci sayıyı gir: ");
-            double y = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine($"Toplama:   {calc.Add(x, y)}");
-            Console.WriteLine($"Çıkarma:   {calc.Subtract(x, y)}");
-            Console.WriteLine($"Çarpma:    {calc.Multiply(x, y)}");
-            try
-            {
-                Console.WriteLine($"Bölme:     {calc.Divide(x, y)}");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Hata: " + ex.Message);
-            }
-        }
-    }
-
     public class Calculator
     {
-        //test comment
-        //test comment 2
-        internal object Add(double x, double y)
+        public double Add(double x, double y)
         {
-            throw new NotImplementedException();
+            return x + y;
         }
 
-        internal object Divide(double x, double y)
+        public double Subtract(double x, double y)
         {
-            throw new NotImplementedException();
+            return x - y;
         }
 
-        internal object Multiply(double x, double y)
+        public double Multiply(double x, double y)
         {
-            throw new NotImplementedException();
+            return x * y;
         }
 
-        internal object Subtract(double x, double y)
+        public double Divide(double x, double y)
         {
-            throw new NotImplementedException();
+            if (y == 0)
+                throw new DivideByZeroException("Bölen sıfır olamaz!");
+            return x / y;
         }
     }
-}
+} // test comment 3
